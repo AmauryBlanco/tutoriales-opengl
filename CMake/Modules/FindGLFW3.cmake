4eefd4716285202a37e49bbd0376a44ff43c976b
Carmelo Marín Abrego <marin.carmelo.abrego@gmail.com>
2016-04-14 10:49:13 -0600

    Renombrar cada uno de los proyectos de la serie de tutoriales OpenGL Moderno

--- CMake/Modules/FindGLFW3.cmake
+++ CMake/Modules/FindGLFW3.cmake
@@ -1,5 +1,7 @@
-find_path(GLFW3_INCLUDE_DIRS GLFW/glfw3.h HINTS $ENV{GLFW3_DIR} PATH_SUFFIXES include)
-find_library(GLFW3_LIBRARY NAMES glfw3 HINTS $ENV{GLFW3_DIR} PATH_SUFFIXES lib64 lib)
+set(GLFW3_DIR $ENV{GLFW3_DIR} CACHE PATH "GLFW3 Directorio de instalacion")
+
+find_path(GLFW3_INCLUDE_DIRS GLFW/glfw3.h HINTS ${GLFW3_DIR}  PATH_SUFFIXES include)
+find_library(GLFW3_LIBRARY NAMES glfw3 HINTS ${GLFW3_DIR} PATH_SUFFIXES lib64 lib)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLFW3 DEFAULT_MSG GLFW3_LIBRARY GLFW3_INCLUDE_DIRS)
